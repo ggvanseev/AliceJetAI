5123be32919f7b0ee929ca6a960b4de6dda94204
ggvanseev <gijsvanseeventer@hotmail.com>
2021-11-05 16:47:55 +0100

    test2

--- analysis/empty.py
+++ analysis/empty.py
@@ -1,2 +1,3 @@
 def test():
+    a = 1
     pass
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # setup.py
+# Run following in terminal: pip install -e .
 from setuptools import setup, find_packages
 
 setup(name="AliceJetAI", version="1.0.0", packages=find_packages())
