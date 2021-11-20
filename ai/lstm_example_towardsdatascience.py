bcfda61a1518a048f570fe8d49bf7b08d8b15d8f
ggvanseev <gijsvanseeventer@hotmail.com>
2021-11-20 17:29:32 +0100

    Added lstm easy (working) example

---
+++ ai/lstm_example_towardsdatascience.py
@@ -0,0 +1,298 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import (
+    MinMaxScaler,
+    StandardScaler,
+    MaxAbsScaler,
+    RobustScaler,
+)
+
+from torch.utils.data import TensorDataset, DataLoader
+import torch
+import torch.nn as nn
+from datetime import datetime
+
+import torch.optim as optim
+
+# Source: https://towardsdatascience.com/building-rnn-lstm-and-gru-for-time-series-using-pytorch-a46e5b094e7b
+# For steps explenation and furthure information see source
+
+
+df = pd.read_csv(r"C:\Users\gijsv\Downloads\archive\PJME_hourly.csv")
+
+df = df.set_index(["Datetime"])
+df.index = pd.to_datetime(df.index)
+if not df.index.is_monotonic:
+    df = df.sort_index()
+
+df = df.rename(columns={"PJME_MW": "value"})
+
+
+def generate_time_lags(df, n_lags):
+    df_n = df.copy()
+    for n in range(1, n_lags + 1):
+        df_n[f"lag{n}"] = df_n["value"].shift(n)
+    df_n = df_n.iloc[n_lags:]
+    return df_n
+
+
+input_dim = 100
+
+df_generated = generate_time_lags(df, input_dim)
+
+df_features = (
+    df.assign(hour=df.index.hour)
+    .assign(day=df.index.day)
+    .assign(month=df.index.month)
+    .assign(day_of_week=df.index.dayofweek)
+    .assign(week_of_year=df.index.week)
+)
+
+
+def onehot_encode(df, onehot_columns):
+    ct = ColumnTransformer(
+        [("onehot", OneHotEncoder(drop="first", categories="auto"), onehot_columns)],
+        remainder="passthrough",
+    )
+    return ct.fit_transform(df)
+
+
+onehot_columns = ["hour"]
+onehot_encoded = onehot_encode(df_features, onehot_columns)
+
+
+def generate_cyclical_features(df, col_name, period, start_num=0):
+    kwargs = {
+        f"sin_{col_name}": lambda x: np.sin(
+            2 * np.pi * (df[col_name] - start_num) / period
+        ),
+        f"cos_{col_name}": lambda x: np.cos(
+            2 * np.pi * (df[col_name] - start_num) / period
+        ),
+    }
+    return df.assign(**kwargs).drop(columns=[col_name])
+
+
+df_features = generate_cyclical_features(df_features, "hour", 24, 0)
+# df_features = generate_cyclical_features(df_features, 'day_of_week', 7, 0)
+# df_features = generate_cyclical_features(df_features, 'month', 12, 1)
+# df_features = generate_cyclical_features(df_features, 'week_of_year', 52, 0)
+
+df_features
+
+
+def feature_label_split(df, target_col):
+    y = df[[target_col]]
+    X = df.drop(columns=[target_col])
+    return X, y
+
+
+def train_val_test_split(df, target_col, test_ratio):
+    val_ratio = test_ratio / (1 - test_ratio)
+    X, y = feature_label_split(df, target_col)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_ratio, shuffle=False
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=val_ratio, shuffle=False
+    )
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+
+X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(
+    df_features, "value", 0.2
+)
+
+scaler = MinMaxScaler()
+X_train_arr = scaler.fit_transform(X_train)
+X_val_arr = scaler.transform(X_val)
+X_test_arr = scaler.transform(X_test)
+
+y_train_arr = scaler.fit_transform(y_train)
+y_val_arr = scaler.transform(y_val)
+y_test_arr = scaler.transform(y_test)
+
+
+batch_size = 64
+
+train_features = torch.Tensor(X_train_arr)
+train_targets = torch.Tensor(y_train_arr)
+val_features = torch.Tensor(X_val_arr)
+val_targets = torch.Tensor(y_val_arr)
+test_features = torch.Tensor(X_test_arr)
+test_targets = torch.Tensor(y_test_arr)
+
+train = TensorDataset(train_features, train_targets)
+val = TensorDataset(val_features, val_targets)
+test = TensorDataset(test_features, test_targets)
+
+train_loader = DataLoader(train, batch_size=batch_size, shuffle=False, drop_last=True)
+val_loader = DataLoader(val, batch_size=batch_size, shuffle=False, drop_last=True)
+test_loader = DataLoader(test, batch_size=batch_size, shuffle=False, drop_last=True)
+test_loader_one = DataLoader(test, batch_size=1, shuffle=False, drop_last=True)
+
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        super(LSTMModel, self).__init__()
+
+        # Defining the number of layers and the nodes in each layer
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+
+        # LSTM layers
+        self.lstm = nn.LSTM(
+            input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+        )
+
+        # Fully connected layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initializing hidden state for first input with zeros
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # Initializing cell state for first input with zeros
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # We need to detach as we are doing truncated backpropagation through time (BPTT)
+        # If we don't, we'll backprop all the way to the start even after going through another batch
+        # Forward propagation by passing in the input, hidden state, and cell state into the model
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+
+        # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
+        # so that it can fit into the fully connected layer
+        out = out[:, -1, :]
+
+        # Convert the final state to our desired output shape (batch_size, output_dim)
+        out = self.fc(out)
+
+        return out
+
+
+class Optimization:
+    def __init__(self, model, loss_fn, optimizer):
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.train_losses = []
+        self.val_losses = []
+
+    def train_step(self, x, y):
+        # Sets model to train mode
+        self.model.train()
+
+        # Makes predictions
+        yhat = self.model(x)
+
+        # Computes loss
+        loss = self.loss_fn(y, yhat)
+
+        # Computes gradients
+        loss.backward()
+
+        # Updates parameters and zeroes gradients
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+
+        # Returns the loss
+        return loss.item()
+
+    def train(self, train_loader, val_loader, batch_size=64, n_epochs=50, n_features=1):
+        model_path = (
+            f'models/{self.model}_{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+        )
+
+        device = (
+            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        )
+
+        for epoch in range(1, n_epochs + 1):
+            batch_losses = []
+            for x_batch, y_batch in train_loader:
+                x_batch = x_batch.view([batch_size, -1, n_features]).to(device)
+                y_batch = y_batch.to(device)
+                loss = self.train_step(x_batch, y_batch)
+                batch_losses.append(loss)
+            training_loss = np.mean(batch_losses)
+            self.train_losses.append(training_loss)
+
+            with torch.no_grad():
+                batch_val_losses = []
+                for x_val, y_val in val_loader:
+                    x_val = x_val.view([batch_size, -1, n_features]).to(device)
+                    y_val = y_val.to(device)
+                    self.model.eval()
+                    yhat = self.model(x_val)
+                    val_loss = self.loss_fn(y_val, yhat).item()
+                    batch_val_losses.append(val_loss)
+                validation_loss = np.mean(batch_val_losses)
+                self.val_losses.append(validation_loss)
+
+            if (epoch <= 10) | (epoch % 50 == 0):
+                print(
+                    f"[{epoch}/{n_epochs}] Training loss: {training_loss:.4f}\t Validation loss: {validation_loss:.4f}"
+                )
+
+        torch.save(self.model.state_dict(), model_path)
+
+
+def evaluate(self, test_loader, batch_size=1, n_features=1):
+    with torch.no_grad():
+        predictions = []
+        values = []
+
+        device = (
+            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        )
+
+        for x_test, y_test in test_loader:
+            x_test = x_test.view([batch_size, -1, n_features]).to(device)
+            y_test = y_test.to(device)
+            self.model.eval()
+            yhat = self.model(x_test)
+            predictions.append(yhat.to(device).detach().numpy())
+            values.append(y_test.to(device).detach().numpy())
+
+    return predictions, values
+
+
+input_dim = len(X_train.columns)
+output_dim = 1
+hidden_dim = 64
+layer_dim = 3
+batch_size = 64
+dropout = 0.2
+n_epochs = 100
+learning_rate = 1e-3
+weight_decay = 1e-6
+
+model_params = {
+    "input_dim": input_dim,
+    "hidden_dim": hidden_dim,
+    "layer_dim": layer_dim,
+    "output_dim": output_dim,
+    "dropout_prob": dropout,
+}
+
+model = LSTMModel(**model_params)
+
+loss_fn = nn.MSELoss(reduction="mean")
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+opt = Optimization(model=model, loss_fn=loss_fn, optimizer=optimizer)
+opt.train(
+    train_loader,
+    val_loader,
+    batch_size=batch_size,
+    n_epochs=n_epochs,
+    n_features=input_dim,
+)
+opt.plot_losses()
+
+predictions, values = opt.evaluate(test_loader_one, batch_size=1, n_features=input_dim)
