9627c551e89174be81a29f7d8de4d03f697fc600
wesselrijk <wesselrijk@gmail.com>
2021-12-17 09:30:37 +0100

    optimization algorithm

--- analysis/qp_training.py
+++ analysis/qp_training.py
@@ -74,109 +74,212 @@ from functions.optimization_orthogonality_constraints import optimization
 from ai.model_lstm import LSTMModel
 
 
-def train(file_name):
+file_name = "samples/JetToyHIResultSoftDropSkinny.root"
 
-    # Variables:
-    batch_size = 210
+# Variables:
+batch_size = 210
 
-    output_dim = 1
-    layer_dim = 1
-    dropout = 0.2
-    n_epochs = 4
-    learning_rate = 1e-3
-    weight_decay = 1e-6
+output_dim = 1
+layer_dim = 1
+dropout = 0.2
+n_epochs = 4
+learning_rate = 1e-3
+weight_decay = 1e-6
 
-    eps = 1e-2  # test value for convergence
+eps = 1e-2  # test value for convergence
 
-    # Load and filter data for criteria eta and jetpt_cap
-    _, _, g_recur_jets, _ = load_n_filter_data(file_name)
-    g_recur_jets = format_ak_to_list(g_recur_jets)
+# Load and filter data for criteria eta and jetpt_cap
+_, _, g_recur_jets, _ = load_n_filter_data(file_name)
+g_recur_jets = format_ak_to_list(g_recur_jets)
 
-    # only use g_recur_jets
-    train_data, dev_data, test_data = train_dev_test_split(
-        g_recur_jets, split=[0.8, 0.1]
-    )
+# only use g_recur_jets
+train_data, dev_data, test_data = train_dev_test_split(
+    g_recur_jets, split=[0.8, 0.1]
+)
 
-    train_data, track_jets_train_data = branch_filler(train_data, batch_size=batch_size)
-    dev_data, track_jets_dev_data = branch_filler(dev_data, batch_size=batch_size)
+train_data, track_jets_train_data = branch_filler(train_data, batch_size=batch_size)
+dev_data, track_jets_dev_data = branch_filler(dev_data, batch_size=batch_size)
 
-    # Only use train and dev data for now
-    scaler = (
-        MinMaxScaler()
-    )  # Note this has to be saved with the model, to ensure data has the same form.
-    train_loader = lstm_data_prep(
-        data=train_data, scaler=scaler, batch_size=batch_size, fit_flag=True
-    )
-    val_loader = lstm_data_prep(data=dev_data, scaler=scaler, batch_size=batch_size)
+# Only use train and dev data for now
+scaler = (
+    MinMaxScaler()
+)  # Note this has to be saved with the model, to ensure data has the same form.
+train_loader = lstm_data_prep(
+    data=train_data, scaler=scaler, batch_size=batch_size, fit_flag=True
+)
+val_loader = lstm_data_prep(data=dev_data, scaler=scaler, batch_size=batch_size)
 
-    input_dim = len(train_data[0])
-    hidden_dim = batch_size
+input_dim = len(train_data[0])
+hidden_dim = batch_size
 
-    model_params = {
-        "input_dim": input_dim,
-        "hidden_dim": hidden_dim,
-        "layer_dim": layer_dim,
-        "output_dim": output_dim,
-        "dropout_prob": dropout,
-    }
+model_params = {
+    "input_dim": input_dim,
+    "hidden_dim": hidden_dim,
+    "layer_dim": layer_dim,
+    "output_dim": output_dim,
+    "dropout_prob": dropout,
+}
 
-    # lstm model
-    lstm_model = LSTMModel(**model_params)
+# lstm model
+lstm_model = LSTMModel(**model_params)
 
-    # svm model
-    svm_model = OneClassSVM(nu=0.5, gamma=0.35, kernel="linear")
+# svm model
+svm_model = OneClassSVM(nu=0.5, gamma=0.35, kernel="rbf")
 
-    # path for model - only used for saving
-    # model_path = f'models/{lstm_model}_{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+# path for model - only used for saving
+# model_path = f'models/{lstm_model}_{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
 
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    k = -1
-    while k < 20:  # TODO, (kappa(theta_next, alpha_next) - kappa(theta, alpha) < eps)
 
-        # track branch number for tracking what jet_track array to use
-        k += 1
+def lstm_results(lstm_model, train_loader):
+    h_bar_list = []
 
-        h_bar_list = []
+    i = 0
+    for x_batch, y_batch in train_loader:
+        jet_track_local = track_jets_train_data[i]
+        i += 1
 
-        i = 0
-        for x_batch, y_batch in train_loader:
-            jet_track_local = track_jets_train_data[i]
-            i += 1
+        x_batch = x_batch.view([batch_size, -1, model_params["input_dim"]]).to(
+            device
+        )
+        y_batch = y_batch.to(device)
 
-            x_batch = x_batch.view([batch_size, -1, model_params["input_dim"]]).to(
-                device
-            )
-            y_batch = y_batch.to(device)
+        ### Train step
+        # set model to train
+        lstm_model.train()
 
-            ### Train step
-            # set model to train
-            lstm_model.train()
+        # Makes predictions
+        yhat, hn, w, r, b = lstm_model(x_batch)
 
-            # Makes predictions
-            yhat, hn, w, r, b = lstm_model(x_batch)
+        # get mean pooled hidden states
+        h_bar = hn[:, jet_track_local]
 
-            # get mean pooled hidden states
-            h_bar = hn[:, jet_track_local]
+        # h_bar_list.append(h_bar) # TODO, h_bar is not of fixed length! solution now: append all to list, then vstack the list to get 2 axis structure
+        h_bar_list.append(h_bar)
+        
+        # also append w, r and b values
 
-            h_bar_list.append(h_bar)
+        # a =[hn.T[x] for x in jet_track][0][i,:].cpu().detach().numpy() selects i-th "mean pooled output"
+        # a.dot(a) = h.T * h = scalar
+    return torch.vstack([h_bar[0] for h_bar in h_bar_list])
 
-            # a =[hn.T[x] for x in jet_track][0][i,:].cpu().detach().numpy() selects i-th "mean pooled output"
-            # a.dot(a) = h.T * h = scalar
 
-        # W, R, b = get_weights(model=lstm_model, batch_size=batch_size)
-        h_bar_list = [h_bar.detach().numpy() for h_bar in h_bar_list]
-        svm_model.fit(h_bar_list)
-        alphas = np.abs(svm_model.dual_coef_)
-        a_idx = svm_model.support_
+def lstm_results_np(lstm_model, train_loader):
+    h_bar_list = lstm_results(lstm_model, train_loader)
+    return np.array([h_bar.detach().numpy() for h_bar in h_bar_list])
 
-        W_next, R_next, b_next = optimization(lstm_model, alphas, a_idx, learning_rate)
 
-        """
-        # get rho?
-        for i in range(h_bar_list):
-            rho = 0
-            for j in range(h_bar_list):
-                rho += alpha[j] * alpha[i] * h_bar_list[j] * h_bar_list[i]
-        """
+def kappa(alphas, a_idx, h_list):
+    out = 0
+    for idx1, i in enumerate(a_idx):
+        for idx2, j in enumerate(a_idx):
+            out += 0.5 * alphas[0, idx1] * alphas[0, idx2] * (h_list[i].T @ h_list[j])
+    return out
+
+
+def diff_argument(lstm_model, train_loader, alphas, a_idx, lr, W=None, R=None, b=None):
+    
+    h_list = lstm_results(lstm_model, train_loader)
+    
+    with torch.no_grad():
+        if W:
+            delta = W - lr * W
+            lstm_model.lstm.weight_ih_l0.copy_(delta)
+        elif R:
+            delta = R - lr * R
+            lstm_model.lstm.weight_hh_l0.copy_(delta)
+        elif b:
+            lstm_model.lstm.weight_ih_l0 
+    
+    h_list_new = lstm_results(lstm_model, train_loader)
+    
+    return (kappa(alphas, a_idx, h_list) - kappa(alphas, a_idx, h_list_new) ) / delta
+
+
+def delta_func(lstm_model, train_loader, h_list, parm, parm_name, mu):
+    
+    delta = parm - mu * parm
+    with torch.no_grad():
+        lstm_model_new = lstm_model
+        if parm_name == 'W':
+            lstm_model_new.lstm.weight_ih_l0.copy_(delta)
+        elif parm_name == 'R':
+            lstm_model_new.lstm.weight_hh_l0.copy_(delta)
+        elif parm_name == 'b':
+            lstm_model_new.lstm.bias_hh_l0.copy_(delta)
+    h_list_new = lstm_results(lstm_model_new, train_loader)
+    return ( kappa(alphas, a_idx, h_list) - kappa(alphas, a_idx, h_list_new) ) / delta
+
+
+def optimization(lstm_model, train_loader, alphas, a_idx, mu):
+
+    # obtain W, R and b from current h
+    # W = h.parameters
+    # R = h.parameters
+    # b = h.parameters
+    #W, R, b = get_weights(lstm_model, batch_size=len())
+    #dh_list = np.diff(h_list)
+    #dW_list = np.diff()
+
+    h_list = lstm_results(lstm_model, train_loader)
+
+    # derivative of function e.g. F = (25) from Tolga
+    W = lstm_model.lstm.weight_ih_l0
+    G = delta_func(lstm_model, train_loader, h_list, W, 'W', mu)
+    #G = np.diff(kappa(alphas, a_idx, h_list) - kappa(alphas, a_idx, h_list_new) ) / np.diff(W - mu* W)
+    #G = np.gradient(lambda W: diff_argument(lstm_model, train_loader, alphas, a_idx, W=W), dW)
+    A = G @ W.T - W @ G.T
+    I = torch.eye(W.shape[0])
+    # next point from Crank-Nicolson-like scheme
+    W_next = (I + mu / 2 * A) ** (-1) @ (I - mu / 2 * A) @ W
+
+    # same for R and b
+    R = lstm_model.lstm.weight_hh_l0
+    G = delta_func(lstm_model, train_loader, h_list, R, 'R', mu)
+    #dR = R - mu * R
+    #with torch.no_grad():
+    #    lstm_model_new = lstm_model
+    #    lstm_model_new.lstm.weight_hh_l0.copy_(dR)
+    #G = np.gradient(lambda R: diff_argument(lstm_model, train_loader, alphas, a_idx, R=R), dW)
+    #G = (kappa(alphas, a_idx, h_list) - kappa(alphas, a_idx, h_list_new) ) / dW
+    A = G @ R.T - R @ G.T
+    I = torch.eye(R.shape[0])
+    R_next = (I + mu / 2 * A) ** (-1) @ (I - mu / 2 * A) @ R
+
+    # TODO: two bias values: lstm_model.lstm_bias_hh_l0 and lstm_model.lstm_bias_ih_l0
+    b = lstm_model.lstm_bias_hh_l0
+    #G = np.gradient(lambda b: diff_argument(lstm_model, train_loader, alphas, a_idx, b=b), dW)
+    G = delta_func(lstm_model, train_loader, h_list, b, 'b', mu)
+    A = G @ b.T - b @ G.T
+    I = torch.eye(b.shape[0])
+    b_next = (I + mu / 2 * A) ** (-1) @ (I - mu / 2 * A) @ b
+
+    return W_next, R_next, b_next
+
+
+### ALGORITHM START ###
+k = -1
+while k < 20:  # TODO, (kappa(theta_next, alpha_next) - kappa(theta, alpha) < eps)
+
+    # track branch number for tracking what jet_track array to use
+    k += 1
+
+    # W, R, b = get_weights(model=lstm_model, batch_size=batch_size)
+    #h_bar_list = [[h_bar.detach().numpy()[0] for h_bar in h_bar_list]]
+    h_bar_list_numpied = lstm_results_np(lstm_model, train_loader)
+    svm_model.fit(h_bar_list_numpied)
+    alphas = np.abs(svm_model.dual_coef_)
+    a_idx = svm_model.support_
+
+    W_next, R_next, b_next = optimization(lstm_model, train_loader, alphas, a_idx, learning_rate)
+    
+
+    """
+    # get rho?
+    for i in range(h_bar_list):
+        rho = 0
+        for j in range(h_bar_list):
+            rho += alpha[j] * alpha[i] * h_bar_list[j] * h_bar_list[i]
+    """
 
