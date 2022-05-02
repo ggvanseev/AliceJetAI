aba4e8c93864ad53fba7c274db35af126ff1e6db
wesselrijk <wesselrijk@gmail.com>
2022-05-02 09:55:23 +0200

    pre merge NH update

--- functions/data_loader.py
+++ functions/data_loader.py
@@ -14,6 +14,7 @@ import branch_names as na
 from typing import Tuple
 import branch_names as na
 import numpy as np
+import random
 
 
 def select_non_empty_branches(branches, non_empty_key):
@@ -54,6 +55,122 @@ def flatten_array(branches):
         new_branches[field] = ak.concatenate(branches[field], axis=0)
 
     return ak.Array(new_branches)
+
+
+def load_n_filter_data_single(
+    file_name: str,
+    tree_name: str = na.tree,
+    cut: bool = True,
+    kt_cut: float = None,
+    eta_max: float = 2.0,
+    pt_min: int = 130,
+    jet_recur_branches: list = [na.recur_dr, na.recur_jetpt, na.recur_z],
+) -> Tuple[ak.Array, ak.Array, ak.Array, ak.Array]: 
+   
+    # open file and select jet data and recursive jet data
+    branches = uproot.open(file_name)[tree_name].arrays()
+
+    jets = branches[
+        [
+            na.jetpt,
+            na.jet_eta,
+            na.jet_phi,
+            na.jet_M,
+            na.jet_area,
+        ]
+    ]
+    jets_recur = branches[jet_recur_branches]
+    
+    # Print some info on dataset. Note: Nr of jets is significantly larger than nr of quark/gluon jets.
+    # This is because we only know for sure which jets are quark or gluon jets from the Parton Initiator,
+    # which in turn means that we can at most obtain 1 quark or gluon jet per event.
+    print(f"Number of jets in dataset:\t\t{np.count_nonzero(jets[na.jetpt])}")
+    print(
+        f"Number of gluon jets in dataset:\t{np.count_nonzero(jets[na.parton_match_id] == 21)}"
+    )
+    print(
+        f"Number of quark jets in dataset:\t{np.count_nonzero(abs(jets[na.parton_match_id]) < 7)}"
+    )
+
+    # select quark and gluon jet data
+    
+    g_jets = random.rand()
+    
+    # select quark and gluon jet data
+    g_jets = jets[:0.5*len(jets)]
+    q_jets = jets[0.5*len(jets):]
+
+    # select recursive quark and gluon jet data
+    g_jets_recur = jets_recur[:0.5*len(jets)]
+    q_jets_recur = jets_recur[0.5*len(jets):]
+
+    # apply cuts: -2 < eta < 2 and jet_pt > 130 GeV
+    if cut:
+        print("Applying cuts: -2 < eta < 2 and jet_pt > 130 GeV")
+        g_jets_recur = g_jets_recur[
+            (abs(g_jets[na.jet_eta]) < eta_max) & (g_jets[na.jetpt] > pt_min)
+        ]
+        q_jets_recur = q_jets_recur[
+            (abs(q_jets[na.jet_eta]) < eta_max) & (q_jets[na.jetpt] > pt_min)
+        ]
+        g_jets = g_jets[
+            (abs(g_jets[na.jet_eta]) < eta_max) & (g_jets[na.jetpt] > pt_min)
+        ]
+        q_jets = q_jets[
+            (abs(q_jets[na.jet_eta]) < eta_max) & (q_jets[na.jetpt] > pt_min)
+        ]
+        print(
+            f"\tgluon jets left after cuts:\t{np.count_nonzero(g_jets[na.parton_match_id] == 21)}"
+        )
+        print(
+            f"\tquark jets left after cuts:\t{np.count_nonzero(abs(q_jets[na.parton_match_id] < 7))} "
+        )
+
+    # apply kt_cut of kt > 1.0 GeV
+    if kt_cut:
+        print(f"Applying cut: kt > {kt_cut} GeV on all splittings")
+        # get kt values
+        g_jets_kt = (
+            g_jets_recur.sigJetRecur_jetpt
+            * g_jets_recur.sigJetRecur_dr12
+            * g_jets_recur.sigJetRecur_z
+        )
+        q_jets_kt = (
+            q_jets_recur.sigJetRecur_jetpt
+            * q_jets_recur.sigJetRecur_dr12
+            * q_jets_recur.sigJetRecur_z
+        )
+
+        # cut kts
+        g_jets_recur = g_jets_recur[g_jets_kt > kt_cut]
+        q_jets_recur = q_jets_recur[q_jets_kt > kt_cut]
+        print(f"\tgluon splittings cut:\t\t{1 - np.count_nonzero(g_jets_kt[g_jets_kt > kt_cut]) / np.count_nonzero(g_jets_kt):.2%}")
+        print(f"\tquark splittings cut:\t\t{1 - np.count_nonzero(q_jets_kt[q_jets_kt > kt_cut]) / np.count_nonzero(q_jets_kt):.2%}")
+
+
+        # hist gluons TODO keep for possible later analysis: histograms
+        #g_kts_flat = ak.flatten(ak.flatten(g_jets_kt)).to_list()
+        # g_kts_hist = np.histogram(g_kts_flat, bins=range(round(max(g_kts_flat)))) 
+
+        # hist quarks TODO keep for possible later analysis: histograms
+        #q_kts_flat = ak.flatten(ak.flatten(q_jets_kt)).to_list()
+        # q_kts_hist = np.histogram(q_kts_flat, bins=range(round(max(q_kts_flat)))) 
+
+    # remove empty additions from recursive jets and flatten them, i.e. take jet out of event nesting
+    g_jets_recur = select_non_empty_branches(
+        g_jets_recur, non_empty_key=jet_recur_branches[0]
+    )
+
+    g_jets_recur = flatten_array(g_jets_recur)
+
+    q_jets_recur = select_non_empty_branches(
+        q_jets_recur, non_empty_key=jet_recur_branches[0]
+    )
+
+    q_jets_recur = flatten_array(q_jets_recur)
+
+    return g_jets_recur, q_jets_recur
+
 
 
 def load_n_filter_data(
--- storing_results/run_info.p
+++ storing_results/run_info.p
@@ -6,3 +6,4 @@
 18_03_22_1415	on: samples/pendigits/pendigits-orig.tra	runtime: 96.42 s	notes: first succesful test with digits
 18_03_22_1428	on: samples/pendigits/pendigits-orig.tra	runtime: 173.02 s	notes: first succesful test with digits
 19_03_22_1725	on: samples/JetToyHIResultSoftDropSkinny.root	runtime: 34.22 s
+22_04_29_1016	on: samples/JetToyHIResultSoftDropSkinny.root	runtime: 312.08 s	notes: qg with new settings, check if it improves
