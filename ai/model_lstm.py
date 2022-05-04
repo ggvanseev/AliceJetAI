Correct h_bar to hn for backpropagatinon

ai/model_lstm.py
@@ -88,7 +88,7 @@ class LSTMModel(nn.Module):
         # Check if backpropogation is required
         if backpropagation_flag:
             # Do backward to get gradients with respect to hn (to get first part of chain rule, only take derivative of kappa later for algorithm Tolga)
-            h_bar.sum().backward()
+            hn.sum().backward()
 
             # Get parameters to update, save in dict for easy reference.
             if (