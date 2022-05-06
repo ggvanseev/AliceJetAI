117f851fc016fa8e800947d28844a9887b454dd6
ggvanseev <gijsvanseeventer@hotmail.com>
2022-05-06 15:14:26 +0200

    Updated manual_filter

--- analysis/manual_filter.py
+++ analysis/manual_filter.py
@@ -1,8 +1,13 @@
 import torch
-
+from PIL import Image
+import numpy as np
+import win32gui as wg
+from win32gui import GetForegroundWindow
+import win32com.client
 
 job_id = 10206558
-selected_list = [0, 1]
+# If list left empty, will give visual options using terminal
+selected_list = []
 
 
 # load trials results from file and
@@ -11,14 +16,44 @@ trials_test_list = torch.load(
     f"storing_results/trials_test_{job_id}.p", map_location=device
 )
 
+# give user the option to select using the terminal
+if len(selected_list) < 1:
+    n_figures = len(trials_test_list["_trials"])
+
+    # This gets the details of the current window, the one running the program
+    aw = np.zeros(n_figures + 1)
+    aw[0] = GetForegroundWindow()
+    shell = win32com.client.Dispatch("WScript.Shell")
+
+    for i in range(n_figures):
+        with Image.open(f"output/cost_condition_{job_id}/trial_{i}.png") as img:
+            img.show()
+            continue_flag = True
+            while continue_flag:
+                shell.SendKeys("%")
+                wg.SetForegroundWindow(aw[i])
+                get_pass = input("y or n:")
+                if get_pass:
+                    aw[i + 1] = GetForegroundWindow()
+                if get_pass == "y":
+                    selected_list.append(i)
+                    continue_flag = False
+                elif get_pass == "n":
+                    continue_flag = False
+
+
 # select only desired
 selected_trials = dict()
-# Add weird nesting for consitency without manual fitler to not adjust code (no further meaning)
-selected_trials["_trials"] = dict()
+
+# store selection with respect to original
+selected_trials["original_index"] = selected_list
+# save selected trials
+selected_trials["_trials"] = list()
+
 
 for i, j in enumerate(selected_list):
-    selected_trials["_trials"][i] = trials_test_list["_trials"][j]
+    selected_trials["_trials"].append(trials_test_list["_trials"][j])
 
 out_file = f"storing_results/trials_test_manual_filter_{job_id}.p"
 
-torch.save(selected_list, open(out_file, "wb"))
+torch.save(selected_trials, open(out_file, "wb"))
