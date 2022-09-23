9d6229deb63d3c6aeff8ed4ca0d251795ea769b1
wesselrijk <wesselrijk@gmail.com>
2022-09-23 12:19:44 +0200

    NH roc/stacked best hypertraining

--- analysis/get_anomalies.py
+++ analysis/get_anomalies.py
@@ -74,14 +74,15 @@ job_ids = [
     "11474169",
 ]
 job_ids = [
-    "11478120",
-    "11478121",
+    # "11478120",
+    # "11478121",
+    "11120653",
 ]
 
 out_files = [] # if previously created a specific sample, otherwise leave empty
 
-g_percentage = 90 # for evaluation of stacked plots 50%, ROC would be nice to have 90 vs 10 percent
-num = 7 # trial nr.
+g_percentage = 50 # for evaluation of stacked plots 50%, ROC would be nice to have 90 vs 10 percent
+num = 3 # trial nr.
 save_flag = True
 show_distribution_percentages_flag = False
 
@@ -183,11 +184,11 @@ for i, job_id in enumerate(job_ids):
     # ROC_feature_curve_qg(g_jets_recur, q_jets_recur, features, trials, job_id)
     # ROC_feature_curve_qg(g_jets_recur, q_jets_recur, features, trials, job_id, samples="first")
     # ROC_feature_curve_qg(g_jets_recur, q_jets_recur, features, trials, job_id, samples="last")
-    collect_aucs = ROC_curve_qg(g_jets_recur, q_jets_recur, trials, job_id)
-    all_aucs[job_id] = collect_aucs
+    #collect_aucs = ROC_curve_qg(g_jets_recur, q_jets_recur, trials, job_id)
+    #all_aucs[job_id] = collect_aucs
     
-    #stacked_plots_mean_qg(g_anomaly, g_normal, q_anomaly, q_normal, features, job_id)
-    #stacked_plots_mean_qg_sided(g_anomaly, g_normal, q_anomaly, q_normal, features, job_id)
+    stacked_plots_mean_qg(g_anomaly, g_normal, q_anomaly, q_normal, features, job_id)
+    stacked_plots_mean_qg_sided(g_anomaly, g_normal, q_anomaly, q_normal, features, job_id)
     #stacked_plots_splittings_qg(g_anomaly, g_normal, q_anomaly, q_normal, features, job_id)
     #stacked_plots_splittings_qg_sided(g_anomaly, g_normal, q_anomaly, q_normal, features, job_id)
     #stacked_plots_first_entries_qg(g_anomaly, g_normal, q_anomaly, q_normal, features, job_id)
