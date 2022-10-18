a6563bec3f58d4acf498d86a39ae81fc07f661ae
wesselrijk <wesselrijk@gmail.com>
2022-10-18 12:01:50 +0200

    Updated stacked plots, make Lund & Stacked nikhef last time?

--- testing/get_anomalies_lund_jewel.py
+++ testing/get_anomalies_lund_jewel.py
@@ -44,6 +44,9 @@ jets_recur_simple, _ = load_n_filter_data(file_name_simple, kt_cut=kt_cut, dr_cu
 _, _, split_test_data_recur_simple = train_dev_test_split(jets_recur_simple, split=[0.7, 0.1])
 print("Loading data complete")  
 
+# lund plane vac versus simple
+lund_planes(split_test_data_recur_vac, split_test_data_recur_simple, labels=["Unquenched Jets", "Contains Quenched Jets"], info="quenchedness")
+
 
 for i, (job_id, num) in enumerate(zip(job_ids, trial_nrs)):
     
@@ -81,10 +84,7 @@ for i, (job_id, num) in enumerate(zip(job_ids, trial_nrs)):
             lund_planes(normal_vac, anomaly_vac, job_id, trial=num)
         else:
             print("Using simple dataset for", job_id)
-            lund_planes(anomaly_simple, normal_simple, job_id, trial=num)
-        
-        lund_planes(split_test_data_recur_vac, split_test_data_recur_simple, job_id, trial=num, labels=["Unquenched Jets", "Contains Quenched Jets"], info="quenchedness")
-        
+            lund_planes(anomaly_simple, normal_simple, job_id, trial=num)        
         
         for feature in [na.recur_jetpt, na.recur_dr, na.recur_z]:
             # store roc curve plots in designated directory
@@ -99,8 +99,8 @@ for i, (job_id, num) in enumerate(zip(job_ids, trial_nrs)):
                         [ak.firsts(normal_simple[feature]), ak.firsts(anomaly_simple[feature])]]
                 out_file =  out_dir + f"/trial{num}_first_" + feature 
                 title = "Unquenched Samples Versus QGP Samples  - First Splitting"
-                x_label = feature
-                stacked_plot_sided_old(data, title, x_label, out_file, labels=["Unquenched Data", "Quenched Data"])
+                x_label = na.variable_names[feature]
+                stacked_plot_sided_old(data, title, x_label, out_file, titles=["Unquenched Data", "Quenched Data"])
             except ValueError:
                 print(f"Either no normal or no anomalous data for {job_id} trial {num}!")
                 try:
